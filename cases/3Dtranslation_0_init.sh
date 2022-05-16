bcca809796adce3a976131c6b356c246c3d51b23
Tomislav Maric <tomislav.maric@gmx.com>
2022-05-16 17:20:54 +0200

    LLS0LS translation parameterized

---
+++ cases/3Dtranslation_0_init.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./create-study.py -c 3Dtranslation  -p 3Dtranslation.parameter -s 3Dtranslation
---
+++ cases/3Dtranslation_1_run_foamJob.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+./bulkrun 3Dtranslation_00000 "foamJob eval '\
+	blockMesh && decomposePar -force && mpirun -np 4 leiaSetFields -parallel && \
+	mpirun -np 4 leiaLevelSetFoam -parallel'"
