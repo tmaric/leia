bf50148dc9d28658e66f7149d2ca4c90e8c32f27
Julian Reitzel <j.reitzel@outlook.de>
2023-03-01 19:58:16 +0100

    UPDATE: python-package/ scripts/ minor updates and study_0_init.py will write .json and .info

--- cases/study_0_init.py
+++ cases/study_0_init.py
@@ -82,17 +82,34 @@ def create_parameterstudy(args):
         check=True
     )
 
-def create_pyFoam_listvariationfile(args, meta_name):
-    pyFoamVariation_file = meta_name + '.pyFoam-variation'
+def create_pyFoam_variationfile(args, metaname):
+    pyFoamVariation_file = metaname + '.pyFoam-variation'
     run(f"pyFoamRunParameterVariation.py --list-variation {quote(args.casedir)} {quote(args.paramfile)} > {pyFoamVariation_file}",
-        shell=True,
+        shell=True, check=True
         )
-    
-def create_concrete_casesfile(args, meta_name):
-    casesfile = meta_name + '.cases'
-    run(f"find -maxdepth 1 -type d -name '{args.studyname}_[0-9][0-9][0-9][0-9][0-9]*' | sort | xargs -n1 basename > {casesfile}",
+    return pyFoamVariation_file
+
+def create_json_variationfile(metaname, casesfile, pyfoamvariationfile):
+    json_variationfile = metaname + '.json'
+    run(f"case-parameter.py -l {casesfile} convert {pyfoamvariationfile} > {json_variationfile}",
         shell=True)
+    return json_variationfile
+
+    
+def create_concrete_casesfile(args, metaname):
+    casesfile = metaname + '.cases'
+    run(f"find -maxdepth 1 -type d -name '{args.studyname}_[0-9][0-9][0-9][0-9][0-9]*' | sort | xargs -n1 basename > {casesfile}",
+        shell=True, check=True)
     return casesfile
+
+def create_infofile(metaname, studydir):
+    infofile = metaname + '.info'
+    cmd = f"""(
+            echo "Study: {studydir}";
+            echo "Study creation at:";
+            date;
+        ) > {infofile}"""
+    run(cmd, shell=True, check=False)
 
 # def get_concrete_cases(casesfile) -> list[str]:
 #     with open(casesfile, 'r', encoding='utf-8') as file:
@@ -117,7 +134,7 @@ def main():
 
     studydir = create_studydir(args.studyname)
 
-    meta_name = studydir
+    metaname = studydir
 
     # Copies the template case and parameter file into the study. Freezes information of all non-studyparameters to the study.
     cp_templatecase_paramfile_to_studydir(args, studydir)
@@ -126,13 +143,9 @@ def main():
 
     # Generate parameter study simulation cases with pyFoam
     create_parameterstudy(args)
-    
-    # Creates file with almost dictionary structure, connection the variation number to the parameter vector.
-    create_pyFoam_listvariationfile(args, meta_name)
-    
-    # Creates file which lists all concrete cases
-    casesfile = create_concrete_casesfile(args, meta_name)
 
+    # Creates file which lists all concrete cases
+    casesfile = create_concrete_casesfile(args, metaname)
 
     extrafiles = [
         '*.stl',
@@ -140,11 +153,15 @@ def main():
     ]
     cp_extrafiles_to_concrete_cases(extrafiles, args, casesfile)
 
+    create_infofile(metaname, studydir)
+    
+    # Creates file with almost dictionary structure, connection the variation number to the parameter vector.
+    pyFoam_variationfile = create_pyFoam_variationfile(args, metaname)
+    
+    # Creates file similiar to the pyFoamVariation_file, but with case basenames and in json format. Readable by python.
+    create_json_variationfile(metaname, casesfile, pyFoam_variationfile)
 
-    # # Creates file similiar to the pyFoamVariation_file, but with case basenames and in json format. Readable by python.
-    # json_file = casesfile = os.path.join(studydir, studydir + '.json')
-    # run(f"case-parameter.py -l {casesfile} convert {pyFoamVariation_file} > {json_file}",
-    #     shell=True)
+
 
 
 if __name__ == "__main__":
---
+++ python-package/scripts/plot/plot_subdatabase_perturbed.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import plot_database
+from argparse import ArgumentParser, RawTextHelpFormatter
+import pandas as pd
+import os
+
+app_description = \
+"""
+Script and module for postprocessing database-CSV files. 
+Creates timeplots, tables and convergence plots for specified error properties.
+"""
+
+def structs(template, study):
+    # struct = (property, ylabel, title, figname)
+    return [
+        #     (
+        #     ('case', 'E_NARROW_MEAN_GRAD_PSI'),
+        #     r'$ mean(||\nabla \psi|-1|) $',
+        #     template+r' signed-distance error in narrowBand',
+        #     study+'_meanEGradPsi-narrowBand'
+        # ),
+        (
+            ('case', 'E_GEOM_ALPHA'),
+            r'$ E_g $',
+            template+' hex-perturbed CN1'+r' $E_g = \sum_{c \in C} |\Omega_c||\alpha_c(t_e) - \alpha_c(t_0)|$',
+            study+'_Eg'
+        ),
+        (
+            ('case', 'E_VOL_ALPHA_REL'),
+            r'$ E_v $',
+            template+' hex-perturbed CN1'+r' $E_v(t)  = \frac{\left|\sum_{c \in C}\alpha_c(t)|\Omega_c| - \sum_{c \in C} \alpha_c(t_0)|\Omega_c|\right|}{\left|\sum_{c \in C} \alpha_c(t_0)|\Omega_c|\right|}$',
+            study+'_Ev'
+        ),
+    ]
+
+
+def main():
+    parser = ArgumentParser(description=app_description, formatter_class=RawTextHelpFormatter)
+    parser.add_argument("studyCSV", help="The database-CSV file to postprocess.")
+    args = parser.parse_args()
+
+    study_csv = args.studyCSV
+
+    template = os.path.basename(study_csv).split('_')[1]
+    study = os.path.basename(study_csv).rpartition('_')[0]
+
+    study_df = pd.read_csv(study_csv, header=[0,1])
+
+    study_df = study_df[study_df[('studyparameters','PROFILE')] == 'signedDistance']
+    study_df.drop(('studyparameters','PROFILE'), axis='columns', inplace=True)
+    
+    assert study_df.index.is_unique, "Index of study_df is not unique! Would cause errors."
+
+    savedir = f"/home/julian/Masterthesis/meetings/next/{study}"
+    # savedir = f"/home/julian/.local/share/Trash/files/{study}"
+
+    os.makedirs(savedir, exist_ok=True)
+
+    mystructs = structs(template, study)
+    plot_database.runall(study_df, mystructs, savedir)
+
+
+
+if __name__ == '__main__':
+    main()
