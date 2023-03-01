e1c5cecce408f3418803cae7dd03358ce6118de6
Julian Reitzel <jreitzel96@gmail.com>
2023-03-01 14:38:24 +0100

    ADD: OpenFoam src/ applications/ contributions

--- applications/utilities/preProcessing/leiaSetFields/leiaSetFields.C
+++ applications/utilities/preProcessing/leiaSetFields/leiaSetFields.C
@@ -35,6 +35,22 @@ Description
 #include "levelSetImplicitSurfaces.H"
 #include "phaseIndicator.H"
 #include "processorFvPatchField.H"
+#include <math.h>
+
+template<typename Surface> 
+scalar signedDistance_profile(Surface const& surf, vector point)
+{
+    return surf->value(point);
+}
+
+template<typename Surface> 
+scalar tanh_profile(Surface const& surf, vector point)
+{
+    scalar const eps = 1.0;
+    scalar const limit = 0.2; 
+    return limit * Foam::tanh(1/limit * signedDistance_profile(surf, point)/eps);
+}
+
 
 template<typename Surface>  
 void setField
@@ -45,8 +61,41 @@ void setField
 {
     const auto& mesh = psi.mesh();
     const auto& cellCenters = mesh.C();
+
+    const fvSolution& fvSolution (mesh);
+    const dictionary& levelSetDict = fvSolution.subDict("levelSet");
+    const word& profile_word = 
+        levelSetDict.getOrDefault<word>("profile", "signedDistance");
+
+    scalar (*profile)(Surface const&, vector);
+
+    if (profile_word == "tanh")
+    {
+        profile = &tanh_profile;
+        Info << "Level-set profile tanh\n";
+    }
+    else //(profile_word == "signedDistance")
+    {
+        profile = &signedDistance_profile;
+            Info << "Level-set profile signed-distance\n";
+    }
+    // else
+    // {
+    //     InfoErr << "Wrong level-set profile selected.\n";
+    //     exit();
+    //             // FatalIOErrorInLookup
+    //     // (
+    //     //     levelSetDict,
+    //     //     "profile",
+    //     //     systemType,
+    //     //     *dictionaryConstructorTablePtr_
+    //     // ) << exit(FatalIOError);
+    // }
+
+
+
     forAll(psi, cellI)
-        psi[cellI] = surf->value(cellCenters[cellI]);
+        psi[cellI] = profile(surf, cellCenters[cellI]);
     
     const auto& Cf = mesh.Cf();
     const auto& CfBoundaryField = Cf.boundaryField(); 
@@ -56,16 +105,16 @@ void setField
     // Prescribe values at coupled boundary patches.
     forAll(meshBoundary, patchI)
     {
-        const fvPatch& patch = meshBoundary[patchI]; 
-        if (isA<coupledFvPatch>(patch))
-        {
+        // const fvPatch& patch = meshBoundary[patchI]; 
+        // if (isA<coupledFvPatch>(patch))
+        // {
             const auto& CfPatchField = CfBoundaryField[patchI];
             auto& psiPatchField = psiBoundaryField[patchI]; 
             forAll(psiPatchField, faceI)
             {
-                psiPatchField[faceI] = surf->value(CfPatchField[faceI]);
+                psiPatchField[faceI] = profile(surf,CfPatchField[faceI]);
             }
-        }
+        // }
     }
     
     psi.correctBoundaryConditions();
---
+++ src/leiaLevelSet/narrowBand/narrowBand.C
@@ -0,0 +1,75 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2022 AUTHOR,AFFILIATION
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "narrowBand.H"
+#include "addToRunTimeSelectionTable.H"
+
+namespace Foam
+{
+// defineTypeNameAndDebug(narrowBand, false);
+defineRunTimeSelectionTable(narrowBand, Mesh);
+// addToRunTimeSelectionTable(narrowBand, narrowBand, Mesh);
+
+narrowBand::narrowBand(const fvMesh& mesh)
+    :
+        mesh_(mesh)
+{}
+
+autoPtr<narrowBand> narrowBand::New(const fvMesh& mesh)
+{
+    const fvSolution& fvSolution (mesh);
+    const dictionary& levelSetDict = fvSolution.subDict("levelSet");
+    const dictionary& sourceTermDict = levelSetDict.optionalSubDict("SDPLSSourceTerm");
+    const word& narrowBandType = sourceTermDict.getOrDefault<word>("narrowBandType", "none");
+
+    if(narrowBandType == "none")
+    {
+        return autoPtr<narrowBand>();
+    }
+    else
+    {
+        auto* ctorPtr = MeshConstructorTable(narrowBandType);
+
+        if (!ctorPtr)
+        {
+            FatalIOErrorInLookup
+            (
+                fvSolution,
+                "narrowBand",
+                narrowBandType,
+                *MeshConstructorTablePtr_
+            ) << exit(FatalIOError);
+        }
+
+        // Construct the model and return the autoPtr to the object. 
+        return autoPtr<narrowBand>(ctorPtr(mesh));
+    }
+}
+
+} // End namespace Foam
+
+// ************************************************************************* //
