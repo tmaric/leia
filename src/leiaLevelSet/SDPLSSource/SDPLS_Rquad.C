12051da40f614dd87fd71be6144edbe0945c9120
Julian Reitzel <j.reitzel@outlook.de>
2023-03-22 18:53:01 +0100

    ADD: SDPLSSource terms Rlin and Rquad. Rlin: $ S = \psi * (R +1 -|\nabla \psi|) $ Rquad: $ S = \psi * (R +|\nabla \psi|^2 -1) $

---
+++ src/leiaLevelSet/SDPLSSource/SDPLS_Rquad.C
@@ -0,0 +1,78 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2022 AUTHOR,AFFILIATION
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "SDPLS_Rquad.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+// namespace fv
+// {
+
+
+defineTypeNameAndDebug(SDPLS_Rquad, false);
+addToRunTimeSelectionTable(SDPLSSource, SDPLS_Rquad, Dictionary);
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+SDPLS_Rquad::SDPLS_Rquad(const dictionary& dict, const fvMesh& mesh)
+    :
+        SDPLSSource(dict, mesh)
+{}
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+tmp<volScalarField> SDPLS_Rquad::nonLinearPart(const volScalarField& R, const volScalarField& psi, const volVectorField& U) const
+{
+    const fvMesh& mesh = psi.mesh();
+    // const Time& runtime = mesh.time(); 
+
+    return tmp<volScalarField>
+    (
+        new volScalarField
+        (
+            IOobject
+            (
+                word(),
+                fileName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::NO_WRITE,
+                false
+            ),
+            R +pow(mag(fvc::grad(psi)), -2) -1
+        )
+    );
+}
+
+// ************************************************************************* //
+
+// } // End namespace fv
+} // End namespace Foam
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
