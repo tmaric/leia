e441e341b0dc4915e5a659dd019271d058857edf
Julian Reitzel <j.reitzel@outlook.de>
2023-04-28 21:21:47 +0200

    UPDATE: doc/Doxygen with latex packages First tests render successful latex formulas of my thesis.

---
+++ doc/Doxygen/update-macros.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+# If changes were made in ./Macros, call this script to copy the latex packages into your local environment. 
+# Otherwise doxygen might not be able to render latex formulas.
+
+latex -v /dev/null 2>&1 # check if tex live is installed
+if [[ $? -eq 0 ]]; then
+	cp -av ./Macros/* $HOME/texmf/tex/latex/commonstuff/
+else
+	echo "Could not find path to local latex packages."
+fi
