5a6492c3e81a35fd8b3dbeb9c3bcba533474d083
Julian Reitzel <j.reitzel@outlook.de>
2023-04-30 12:45:26 +0200

    UPDATE: src documentation Created filter_latex-doxygen.sh for translating latex text to doxygen text

--- doc/Doxygen/update-macros.sh
+++ doc/Doxygen/update-macros.sh
@@ -3,8 +3,9 @@
 # If changes were made in ./Macros, call this script to copy the latex packages into your local environment. 
 # Otherwise doxygen might not be able to render latex formulas.
 
-latex -v /dev/null 2>&1 # check if tex live is installed
+latex -v > /dev/null 2>&1 # check if tex live is installed
 if [[ $? -eq 0 ]]; then
+	mkdir -p $HOME/texmf/tex/latex/commonstuff
 	cp -av ./Macros/* $HOME/texmf/tex/latex/commonstuff/
 else
 	echo "Could not find path to local latex packages."
