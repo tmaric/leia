db82a71953bde0733a1e514a002dbb4575350fb5
Julian Reitzel <julian.reitzel@stud.tu-darmstadt.de>
2023-05-04 23:54:28 +0200

    ADD: SDPLSSource SDPLS_test Source with S=\psi for testing interface movement with SDPLS

---
+++ src/leiaLevelSet/SDPLSSource/SDPLS_test.C
@@ -0,0 +1,79 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2022 AUTHOR,AFFILIATION
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "SDPLS_test.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+// namespace fv
+// {
+
+
+defineTypeNameAndDebug(SDPLS_test, false);
+addToRunTimeSelectionTable(SDPLSSource, SDPLS_test, Dictionary);
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+SDPLS_test::SDPLS_test(const dictionary& dict, const fvMesh& mesh)
+    :
+        SDPLSSource(dict, mesh)
+{}
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+tmp<volScalarField> SDPLS_test::nonLinearPart(const volScalarField& R, const volScalarField& psi, const volVectorField& U) const
+{
+    const fvMesh& mesh = psi.mesh();
+    // const Time& runtime = mesh.time(); 
+
+    return tmp<volScalarField>
+    (
+        new volScalarField
+        (
+            IOobject
+            (
+                word(),
+                fileName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::NO_WRITE,
+                false
+            ),
+            mesh,
+            dimensioned<scalar>(dimless/dimTime, 1.0)
+        )
+    );
+}
+
+// ************************************************************************* //
+
+// } // End namespace fv
+} // End namespace Foam
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
