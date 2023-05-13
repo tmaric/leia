5fd4279cd434d362a1cca57fe44c6abcfe84662a
Julian Reitzel <jreitzel96@gmail.com>
2023-05-13 18:14:47 +0200

    RM: Cleanup cases/

--- cases/scripts/3Drotation_tet_1_run_foamJob.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-./bulkrun 3Drotation_tet_000 "foamJob eval '\
-	tetMesh && decomposePar -force && mpirun -np 4 leiaSetFields -parallel && \
-	mpirun -np 4 leiaLevelSetFoam -parallel'" 
