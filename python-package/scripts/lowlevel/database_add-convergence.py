151cb8ebdb0ae30144b8e08e297b5bd71dcc7354
Julian Reitzel <julian.reitzel@stud.tu-darmstadt.de>
2023-05-23 17:15:17 +0200

    FIX: python-package/scripts/lowlevel/database_add-convergence.py

--- python-package/scripts/lowlevel/database_add-convergence.py
+++ python-package/scripts/lowlevel/database_add-convergence.py
@@ -71,7 +71,7 @@ def main():
     if time_label not in study_df.columns:
         raise RuntimeError(f"Did not find label '{time_label}' in the columns of the DataFrame")
 
-    errorlabels = list(filter(database.isErrorcolumn, study_df.columns))
+    errorlabels = list(filter(database.isErrorColumn, study_df.columns))
     study_df = add_convergencerates(study_df, 
                                     studyparameters = study_df.columns[study_df.columns.get_loc('studyparameters')], 
                                     refinement_parameter = refinement_parameter, 
