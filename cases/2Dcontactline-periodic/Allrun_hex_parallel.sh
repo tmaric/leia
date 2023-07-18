3cd6fa136707f5f4ad19d2fbb43fbb00cdfc48ef
Julian Reitzel <j.reitzel@outlook.de>
2023-07-18 14:12:04 +0200

    UPDATE: Removed deprecated setting of deltaT within the Allrun scripts

--- cases/2Dcontactline-periodic/Allrun_hex_parallel.sh
+++ cases/2Dcontactline-periodic/Allrun_hex_parallel.sh
@@ -13,14 +13,6 @@ touch "$(basename ${PWD}).foam" && \
 set -o errexit
 blockMesh 
 
-checkMesh | tee log.checkMesh 
-# Fetch minCellVolume from checkMesh
-minVol=$(awk '/Min volume/{print $4}' log.checkMesh | sed 's/\.$//')
-# Calc deltaX with python, because #eval #calc are buggy with small float
-deltaX=$(python3 -c "dX=${minVol}**(1/2); print(f'{dX:e}')")
-echo $deltaX
-# Set deltaX entry in controlDict
-sed -i "/^deltaX\>/s/\<[^ \t]*;$/${deltaX};/" system/controlDict
 
 decomposePar -force
 ${mpi_call} leiaSetFields -parallel
--- cases/3Drotation/Allrun_perturbed_parallel.sh
+++ cases/3Drotation/Allrun_perturbed_parallel.sh
@@ -11,21 +11,12 @@ rm -rf 0 && cp -r 0.org 0
 find 0/ -name "*.template" -delete && \
 touch "$(basename ${PWD}).foam" && \
 set -o errexit
-cp system/fvSchemes.non-orthogonal system/fvSchemes
 blockMesh 
 
 leiaPerturbMesh 
 cp -r 0/polyMesh/ constant/ 
 rm -rf 0/polyMesh
 
-checkMesh | tee log.checkMesh 
-# Fetch minCellVolume from checkMesh
-minVol=$(awk '/Min volume/{print $4}' log.checkMesh | sed 's/\.$//')
-# Calc deltaX with python, because #eval #calc are buggy with small float
-deltaX=$(python3 -c "dX=${minVol}**(1/3); print(f'{dX:e}')")
-echo $deltaX
-# Set deltaX entry in controlDict
-sed -i "/^deltaX\>/s/\<[^ \t]*;$/${deltaX};/" system/controlDict
 
 decomposePar -force
 ${mpi_call} leiaSetFields -parallel
